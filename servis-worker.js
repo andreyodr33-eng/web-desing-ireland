1b01126391ab41003201143912b81ee45502ea37
andreyodr33-eng <andreyodr33@gmail.com>
2026-06-20 11:27:07 +0300

    Add files via upload

---
+++ servis-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'tracker-cache-v1';
+
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-32.png',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    Promise.all([
+      caches.keys().then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+        )
+      ),
+      clients.claim()
+    ])
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+
+  // Для HTML всегда берем свежую версию
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  // Для JS/CSS обновляем кэш в фоне
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, clone);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
